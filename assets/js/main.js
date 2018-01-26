d7979670740c249606675498949d97b3dc9081c8
Jare Huffstutler <jaredhuff85@gmail.com>
2018-01-26 07:06:24 -0700

    Shiit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,21 +8,21 @@ setInterval(function() {
   var screenWidth = screen.width;
   // Screen with is 980px or greater.
   if (screenWidth >= 980) {
-    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/980-' + pageName + '.jpg")';
+    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/980-' + 'index' + '.jpg")';
     // $('img').css('content', 'url("assets/images/980-' + pageName + '.jpg")');
     return true;
   }
 
   // Screen width is between 768px and 979px.
   if (screenWidth >= 768 && screenWidth <= 979) {
-    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/768-' + pageName + '.jpg")';
+    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/768-' + 'index' + '.jpg")';
     // $('img').css('content', 'url("assets/images/768-' + pageName + '.jpg")');
     return true;
   }
 
   // Screen width is 640px or less.
   if (screenWidth <= 640) {
-    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/640-' + pageName + '.jpg")';
+    document.getElementById('responsive-image').style.backgroundImage = 'url("../images/640-' + 'index' + '.jpg")';
     // $('img').css('content', 'url("assets/images/640-' + pageName + '.jpg")');
     return true;
   }
