75ba943e21c64108fabb176436c7f7bf1639862d
Jare Huffstutler <jaredhuff85@gmail.com>
2018-01-26 06:54:39 -0700

    Testing without jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,38 @@
-$(document).ready(function() {
-  // Get the page name for displaying correct images.
-  var pageName = getPageFromURI(document.documentURI);
+// $(document).ready(function() {
+// Get the page name for displaying correct images.
+var pageName = getPageFromURI(document.documentURI);
 
-  // Check the screen size 4 times per second
-  // and show the correct image accordingly.
-  setInterval(function() {
-    var screenWidth = screen.width;
-    // Screen with is 980px or greater.
-    if (screenWidth >= 980) {
-      $('img').css('content', 'url("assets/images/980-' + pageName + '.jpg")');
-      return true;
-    }
-
-    // Screen width is between 768px and 979px.
-    if (screenWidth >= 768 && screenWidth <= 979) {
-      $('img').css('content', 'url("assets/images/768-' + pageName + '.jpg")');
-      return true;
-    }
-
-    // Screen width is 640px or less.
-    if (screenWidth <= 640) {
-      $('img').css('content', 'url("assets/images/640-' + pageName + '.jpg")');
-      return true;
-    }
-
-    return false;
-  }, 250);
-
-  // Get the name of the page from the uri.
-  function getPageFromURI(uri) {
-    var splitURI = uri.split('/');
-    return splitURI[splitURI.length - 1].replace('.html', '');
+// Check the screen size 4 times per second
+// and show the correct image accordingly.
+setInterval(function() {
+  var screenWidth = screen.width;
+  // Screen with is 980px or greater.
+  if (screenWidth >= 980) {
+    document.getElementById().style.backgroundImage = 'url("assets/images/980-' + pageName + '.jpg")';
+    // $('img').css('content', 'url("assets/images/980-' + pageName + '.jpg")');
+    return true;
   }
-});
+
+  // Screen width is between 768px and 979px.
+  if (screenWidth >= 768 && screenWidth <= 979) {
+    document.getElementById().style.backgroundImage = 'url("assets/images/768-' + pageName + '.jpg")';
+    // $('img').css('content', 'url("assets/images/768-' + pageName + '.jpg")');
+    return true;
+  }
+
+  // Screen width is 640px or less.
+  if (screenWidth <= 640) {
+    document.getElementById().style.backgroundImage = 'url("assets/images/640-' + pageName + '.jpg")';
+    // $('img').css('content', 'url("assets/images/640-' + pageName + '.jpg")');
+    return true;
+  }
+
+  return false;
+}, 250);
+
+// Get the name of the page from the uri.
+function getPageFromURI(uri) {
+  var splitURI = uri.split('/');
+  return splitURI[splitURI.length - 1].replace('.html', '');
+}
+// });
