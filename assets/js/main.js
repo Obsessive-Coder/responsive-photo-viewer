9aaedd14190efe82038455df54dc3cc97c650975
Jare Huffstutler <jaredhuff85@gmail.com>
2018-01-31 06:08:56 -0700

    Used jQuery resize event to set the image when the browser is resized

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,44 +4,52 @@ $(window).on("load", function() {
   // Get the page name for displaying correct images.
   pageName = getPageFromURI(document.documentURI);
 });
-setInterval(function() {
-  var screenWidth = $(document).width();
-  var element = document.getElementById('responsive-image');
+
+// setInterval(function() {
+
+$(window).on('load resize', function() {
+  var image = $('#responsive-image');
+  var imageSRC = $(image).attr('src');
+  var imagePath = 'assets/images/';
+  var screenWidth = $(window).width();
 
   // Larger than 980px
   if (screenWidth > 980) {
-    if (element.style.content !== 'url("assets/images/full-' + pageName + '.jpg")') {
-      element.style.content = 'url("assets/images/full-' + pageName + '.png")';
+    if (imageSRC !== imagePath + 'full-' + pageName + '.png') {
+      $(image).attr('src', imagePath + 'full-' + pageName + '.png');
     }
     return true;
   }
 
   //  980px
   if (screenWidth >= 769 && screenWidth <= 980) {
-    if (element.style.content !== 'url("assets/images/980-' + pageName + '.jpg")') {
-      element.style.content = 'url("assets/images/980-' + pageName + '.jpg")';
+    if (imageSRC !== imagePath + '980-' + pageName + '.jpg') {
+      $(image).attr('src', imagePath + '980-' + pageName + '.jpg');
     }
     return true;
   }
 
   // 768px
   if (screenWidth >= 641 && screenWidth <= 768) {
-    if (element.style.content !== 'url("assets/images/768-' + pageName + '.jpg")') {
-      element.style.content = 'url("assets/images/768-' + pageName + '.jpg")';
+    if (imageSRC !== imagePath + '768-' + pageName + '.jpg') {
+      $(image).attr('src', imagePath + '768-' + pageName + '.jpg');
     }
     return true;
   }
 
   // 640px or less.
   if (screenWidth <= 640) {
-    if (element.style.content !== 'url("assets/images/640-' + pageName + '.jpg")') {
-      element.style.content = 'url("assets/images/640-' + pageName + '.jpg")';
+    if (imageSRC !== imagePath + '640-' + pageName + '.jpg') {
+      $(image).attr('src', imagePath + '640-' + pageName + '.jpg');
     }
     return true;
   }
 
   return false;
-}, 250);
+});
+
+// }, 250);
+
 // Get the name of the page from the uri.
 function getPageFromURI(uri) {
   var splitURI = uri.split('/');
