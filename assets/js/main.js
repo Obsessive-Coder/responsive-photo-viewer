2d2ec425fa3bf740c4a51420d66c5997466cdde8
Jare Huffstutler <jaredhuff85@gmail.com>
2018-01-31 05:36:06 -0700

    Added full width images to js and updated all if statements to properly adjust to all 4 images for each site.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,38 @@
 var pageName;
 
-$(window).on("load", function () {
+$(window).on("load", function() {
   // Get the page name for displaying correct images.
   pageName = getPageFromURI(document.documentURI);
 });
-setInterval(function () {
+setInterval(function() {
   var screenWidth = $(document).width();
   var element = document.getElementById('responsive-image');
 
-  // Screen with is 980px or greater.
-  if (screenWidth >= 980) {
+  // Larger than 980px
+  if (screenWidth > 980) {
+    if (element.style.content !== 'url("assets/images/full-' + pageName + '.jpg")') {
+      element.style.content = 'url("assets/images/full-' + pageName + '.png")';
+    }
+    return true;
+  }
+
+  //  980px
+  if (screenWidth >= 769 && screenWidth <= 980) {
     if (element.style.content !== 'url("assets/images/980-' + pageName + '.jpg")') {
       element.style.content = 'url("assets/images/980-' + pageName + '.jpg")';
     }
     return true;
   }
 
-  // Screen width is between 768px and 979px.
-  if (screenWidth >= 768 && screenWidth <= 979) {
+  // 768px
+  if (screenWidth >= 641 && screenWidth <= 768) {
     if (element.style.content !== 'url("assets/images/768-' + pageName + '.jpg")') {
       element.style.content = 'url("assets/images/768-' + pageName + '.jpg")';
     }
     return true;
   }
 
-  // Screen width is 640px or less.
+  // 640px or less.
   if (screenWidth <= 640) {
     if (element.style.content !== 'url("assets/images/640-' + pageName + '.jpg")') {
       element.style.content = 'url("assets/images/640-' + pageName + '.jpg")';
